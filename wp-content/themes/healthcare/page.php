e063274f73ab0075aa938c54161d0084ee9040f4
lthanhbinh <lthanhbinh@tma.com.vn>
2018-12-11 14:06:49 +0700

    aaaaa

---
+++ wp-content/themes/healthcare/page.php
@@ -0,0 +1,18 @@
+<?php get_header()?>
+<div ng-init="loadData()">
+
+
+
+    <section>
+      <div class="container">
+        <div class="section-header">
+            <h2 class="section-title text-center wow fadeInDown animated" style="visibility: visible; animation-name: fadeInDown;">Chuyên khoa</h2>
+        </div>
+        <div class="row">
+          <!-- các icon chuyên khoa, bấn vào icon link đến bài viết được thiết lập. -->
+        </div>
+      </div>
+    </section>
+
+</div>
+<?php get_footer();?>
