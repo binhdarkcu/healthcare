355b894519c6b331aea49ea68aced5461c5acd83
lthanhbinh <lthanhbinh@tma.com.vn>
2018-12-11 17:02:51 +0700

    Update icons

--- wp-content/themes/healthcare/footer.php
+++ wp-content/themes/healthcare/footer.php
@@ -12,19 +12,31 @@
     <!----><footer id="footer" ng-if="!isLoading" class="" style="">
         <div class="container">
             <div class="row">
-                <div class="col-sm-6 footer-left">
+                <div class="col-md-4 footer-left">
                     <!--Thông tin công ty vị trí google map -->
+                    <div class="row">
+                        <div>
+                            <p>Địa chỉ</p>
+                            <div id="map" style="width: 100%;"><div style="width: 100%"><iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=10.7768128,106.6320116&amp;q=L%C5%A9y%20B%C3%A1n%20B%C3%ADch%2C%20T%C3%A2n%20Ph%C3%BA%2C%20H%E1%BB%93%20Ch%C3%AD%20Minh%2C%20Vietnam+(HealthCare)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/map-my-route/">Draw map route</a></iframe></div><br /></div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-sm-2 footer-mid">
+                <div class="col-md-4 footer-mid">
                   <!--facebook
                       zalo
                       Instagram
                       youtube
                       App mobile
                       -->
+                      <div>
+                          <p>Social Icon</p>
+                      </div>
                 </div>
-                <div class="col-sm-6 footer-right">
+                <div class="col-md-4 footer-right">
                   <!--Thời gian làm việc các chuyên khoa-->
+                  <div>
+                      <p>Thời gian làm việc các chuyên khoa</p>
+                  </div>
                 </div>
             </div>
         </div>
@@ -68,15 +80,15 @@
                 }
             );
         }); // end ready
-        function initMap() {
-            // The location of Uluru
-            var uluru = {lat: -25.344, lng: 131.036};
-            // The map, centered at Uluru
-            var map = new google.maps.Map(
-                document.getElementById('map'), {zoom: 4, center: uluru});
-            // The marker, positioned at Uluru
-            var marker = new google.maps.Marker({position: uluru, map: map});
-        }
+        // function initMap() {
+        //     // The location of Uluru
+        //     var uluru = {lat: -25.344, lng: 131.036};
+        //     // The map, centered at Uluru
+        //     var map = new google.maps.Map(
+        //         document.getElementById('map'), {zoom: 4, center: uluru});
+        //     // The marker, positioned at Uluru
+        //     var marker = new google.maps.Marker({position: uluru, map: map});
+        // }
     </script>
 
     <script>
